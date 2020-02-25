045e64611658bc2da2f23af92943dc364106cc34
scuestam <sebastian.cuesta.molto@gmail.com>
2020-02-25 10:25:02 +0100

    Arreglada la vista de la ventana VistaFTP, se abria la vista aunque si conectaba

--- src/bocateria/Main.java
+++ src/bocateria/Main.java
@@ -364,7 +364,7 @@ public class Main extends Application {
         try {
             // Load the fxml file and create a new stage for the popup dialog.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("vista/vistaFTP.fxml"));
+            loader.setLocation(Main.class.getResource("vista/VistaFTP.fxml"));
             AnchorPane page = loader.load();
 
             // Create the dialog Stage.
